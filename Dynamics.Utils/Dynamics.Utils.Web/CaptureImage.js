2cd2ebe42125b9be25e8d0f514ba6eede0298424
rtebar <ramontebar@gmail.com>
2018-01-22 13:16:03 +0000

    New JavaScript function GetPhoto to capture images using the new Xrm.Device namespace in Dynamics 365 version 9.

---
+++ Dynamics.Utils/Dynamics.Utils.Web/CaptureImage.js
@@ -0,0 +1,28 @@
+﻿function GetPhoto(recordId, entityType) {
+    if (recordId == null || entityType == null)
+        return;
+
+    Xrm.Device.captureImage().then(
+        function (imageContent) {
+            if (imageContent != null) {
+                Xrm.WebApi.updateRecord(entityType, recordId, { "entityimage": imageContent.fileContent }).then(
+                    function (result) {
+                        Xrm.Navigation.openAlertDialog({ "text": "Done! Form is going to be refreshed" }).then(
+                            function () {
+                                Xrm.Navigation.openForm({ "entityId": recordId, "entityName": entityType, "openInNewWindow": false })
+                            }
+                        );
+                    }
+                    ,
+                    function (error) {
+                        Xrm.Navigation.openErrorDialog({ "message": "Upps...we couldn't update the record. Error Message: " + error.message });
+                    }
+                );
+            }
+        }
+        ,
+        function (error) {
+            Xrm.Navigation.openErrorDialog({ "message": "Upps...we couldn't capture the image. Error Message: " + error.message });
+        }
+    );
+}
