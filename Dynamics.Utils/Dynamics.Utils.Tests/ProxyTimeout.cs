a3d49c9f6a5f1878b7bac47bc8d6e2e83cd524f7
Ramon Tebar <contact@ramontebar.com>
2020-03-18 08:38:23 +0000

    - Add new test to check CrmServiceClient OrganizationWebProxyClient timeout. - Update .NET reference and Nuget package references in plugin project.

--- Dynamics.Utils/Dynamics.Utils.Tests/ProxyTimeout.cs
+++ Dynamics.Utils/Dynamics.Utils.Tests/ProxyTimeout.cs
@@ -11,16 +11,37 @@ namespace Dynamics.Utils.Tests
     public class ProxyTimeout
     {
         [TestMethod]
-        public void CreateTaskWithTimeout()
+        public void OrganizationServiceProxyTimeout()
         {
-            string dynamicsConnectionString = ConfigurationManager.ConnectionStrings["DynamicsConnectionString"].ConnectionString;
-            CrmServiceClient crmServiceClient = new CrmServiceClient(dynamicsConnectionString);
-            
-            crmServiceClient.OrganizationServiceProxy.Timeout = new TimeSpan(0, 0, 10);
+            string connectionString =
+                "AuthType=Office365;" +
+                "Url=https://mvp20200218.crm11.dynamics.com;" +
+                "Username=ramon@mvp20200218.onmicrosoft.com;" +
+                "Password=[PASSWORD];";
+
+            CrmServiceClient crmServiceClient = new CrmServiceClient(connectionString);
+            crmServiceClient.OrganizationServiceProxy.Timeout = new TimeSpan(0, 0, 15);
 
             Guid taskid = crmServiceClient.Create(new Entity("task"));
 
             crmServiceClient.Retrieve("task",taskid, new Microsoft.Xrm.Sdk.Query.ColumnSet(false));
+        }
+
+        [TestMethod]
+        public void OrganizationWebProxyClientTimeOut()
+        {
+            string connectionString =
+                "authtype=ClientSecret;" +
+                "url=https://mvp20200218.crm11.dynamics.com;" +
+                "clientid=6a268676-1043-4b1e-b1d8-d89b7859eeb5;" +
+                "clientsecret=[SECRET]";
+
+            CrmServiceClient crmServiceClient = new CrmServiceClient(connectionString);
+            crmServiceClient.OrganizationWebProxyClient.InnerChannel.OperationTimeout = new TimeSpan(0, 0, 15);
+
+            Guid taskid = crmServiceClient.Create(new Entity("task"));
+
+            crmServiceClient.Retrieve("task", taskid, new Microsoft.Xrm.Sdk.Query.ColumnSet(false));
         }
     }
 }
